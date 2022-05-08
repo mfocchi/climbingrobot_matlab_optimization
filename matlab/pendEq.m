2a26df5a4695ec39ee7bea62d4241744f1c794b6
Mohamed Bensaadallah <mohamed.bensaadallah@studenti.unitn.it>
2022-05-08 22:47:11 +0000

    pendEq.m 3D animation plot added

--- matlab/pendEq.m
+++ matlab/pendEq.m
@@ -1,24 +1,98 @@
+clear all
+clc
+close all
+
 global dmin; %Minimum distance from the mointain wall
 dmin = 0.5;
+
 global dtol; %Tolerance for binding reaction of the wall
 dtol = 1e-05;
 
 global delta_duration;
 delta_duration = 1e-3;
-global Fu0;
+
+global Fu0;       
 Fu0 = 1000;
+
 global Ft0;
 Ft0 = 100;
-m = 10;
+
+m = 10;   % Mass [kg]
+
 global Tsim;
-Tsim = 10;
-tspan = [0:0.0001: Tsim];
-x0 = [asin(dmin/5); 0; 0; 0; 5; 0];
-[t,x] = ode23(@(t,x) diffEq(t,x,m,@Fr,@Fu, @Ft), tspan, x0);
+Tsim = 10;    % Simulation Time
+tspan = [0:0.01: Tsim];
+
+%Initial Conditions
+x0 = [asin(dmin/5); 0; 0; 0; 5; 0]; %[theta ;d/dt(theta) ;phi ;d/dt(phi) ;l ;d/dt(l)] 
+
+%%Simulation:
+
+%Solve differential equations
+[t,x] = ode23(@(t,x) diffEq(t,x,m,@Fr,@Fu, @Ft), tspan, x0); 
+
+%Coordinates
 X = x(:,5).*cos(x(:,3)).*sin(x(:,1));
 Y = x(:,5).*sin(x(:,3)).*sin(x(:,1));
 Z = -x(:,5).*cos(x(:,1));
-plot3(X,Y,Z)
+% figure(1)
+%plot3(X,Y,Z)
+% comet3(X,Y,Z)      % Plot 3D slow motion curve using Comet3
+
+%% 3D plot Animation
+
+% Min-max axis
+min_x = min(X)-3 ; 
+max_x = max(X)+3 ;
+min_y = min(Y)-3 ;
+max_y = max(Y)+3 ;
+min_z = min(Z)-2;
+max_z = 0;
+
+figure(2)
+% set(gcf,'Position',[50 50 1280 720]) % YouTube: 720p
+% set(gcf,'Position',[50 50 854 480]) % YouTube: 480p
+set(gcf,'Position',[50 50 640 640]) % Social
+
+% Create and open video writer object
+v = VideoWriter('spherical_pendulum.mp4');
+v.Quality = 100;
+open(v);
+    
+hold on ; grid on ; axis equal
+set(gca,'CameraPosition',[42.0101   30.8293   16.2256])
+set(gca,'XLim',[min_x max_x])
+set(gca,'YLim',[min_y max_y])
+set(gca,'ZLim',[min_z max_z])
+set(gca,'XTickLabel',[],'YTickLabel',[],'ZTickLabel',[])
+
+%Loop for animation
+for i = 1:length(X)
+    
+    cla
+    % Vertical line
+    plot3([0 0],[0 0],[0 -x0(5)],'k--')
+    % Point fix
+    p = plot3(0,0,0,'Marker','*','Color','k','MarkerSize',10);
+    % Pendulum trajectory
+    plot3(X(1:i),Y(1:i),Z(1:i),'b')
+    % Pendulum rod
+    plot3([0 X(i)],[0 Y(i)],[0 Z(i)],'r')
+    % Pendulum sphere
+    plot3(X(i),Y(i),Z(i),'Marker','o','Color','k','MarkerFaceColor','r','MarkerSize',10);
+    % Projections
+    plot3(min_x*ones(1,i),Y(1:i),Z(1:i),'g')
+    plot3(X(1:i),min_y*ones(1,i),Z(1:i),'g')
+    plot3(X(1:i),Y(1:i),min_z*ones(1,i),'g')
+    
+    frame = getframe(gcf);
+%   writeVideo(v,frame);
+    
+end
+
+close(v);
+
+%%Functions used in the main program
 
 %Frist component brake. Second component force.
 function [fr] = Fr(t)
@@ -55,14 +129,19 @@ end
 function [dxdt] = diffEq(t,x, m, Fr, Fun ,Fut)
 global dmin;
 global dtol;
+
 %x = [theta, dottheta, phi, dotphi, l, dotl]
-g = 9.8;
+
+g = 9.8;   %Gravity   [m/s2]
+
+%Retrieving states
 theta = x(1);
 dtheta = x(2);
 phi = x(3);
 dphi = x(4);
 l = x(5);
 dl = x(6);
+
 thmin = asin(dmin/l);
 thtol = asin(dtol/l);
 FR = Fr(t);
@@ -104,4 +183,6 @@ dxdt = [dtheta; ddtheta; dphi; ddphi; dl; ddl];
 if(FR(1)<0.99)
 display(dxdt)
 end
+
 end
+
