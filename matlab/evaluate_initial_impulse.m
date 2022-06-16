918a72ede7a60716f5c4ccd97cc74b554155ff1c
Michele Focchi <michele.focchi@iit.it>
2022-06-16 09:53:41 +0200

    simplifying computation of initial impulse

--- matlab/evaluate_initial_impulse.m
+++ matlab/evaluate_initial_impulse.m
@@ -17,29 +17,31 @@ function [Fun , Fut] = evaluate_initial_impulse(x, t)
         a_25 = x(12);
     end
 
-       
-    if (OLD_FORMULATION)          
-        
-       if (POLY_TYPE) %quintic
-            theta0 = a_10 + a_11*t + a_12*t.^2 +  a_13*t.^3 + a_14*t.^4 + a_15*t.^5;
-            thetad0 =  a_11 + 2*a_12*t + 3*a_13*t.^2 + 4*a_14*t.^3 + 5*a_15*t.^4 ;           
-            phid =  a_21 + 2*a_22*t  + 3*a_23*t.^2 + 4*a_24*t.^3 + 5*a_25*t.^4 ;
-          
-       else
-            theta0 = a_10 + a_11*t + a_12*t.^2 +  a_13*t.^3;
-            thetad0 =  a_11 + 2*a_12*t + 3*a_13*t.^2;
-            phid0 =  a_21 + 2*a_22*t  + 3*a_23*t.^2;
-
-        end
-   
-    else
-     
-        zd0 =  a_11 + 2*a_12*t + 3*a_13*t.^2;
-        phid0 =  a_21 + 2*a_22*t  + 3*a_23*t.^2;
-
-    end
-    
-    %evaluate inpulse
+%        
+%     if (OLD_FORMULATION)          
+%         
+%        if (POLY_TYPE) %quintic
+%             theta0 = a_10 + a_11*t + a_12*t.^2 +  a_13*t.^3 + a_14*t.^4 + a_15*t.^5;
+%             thetad0 =  a_11 + 2*a_12*t + 3*a_13*t.^2 + 4*a_14*t.^3 + 5*a_15*t.^4 ;           
+%             phid =  a_21 + 2*a_22*t  + 3*a_23*t.^2 + 4*a_24*t.^3 + 5*a_25*t.^4 ;
+%           
+%        else
+%             theta0 = a_10 + a_11*t + a_12*t.^2 +  a_13*t.^3;
+%             thetad0 =  a_11 + 2*a_12*t + 3*a_13*t.^2;
+%             phid0 =  a_21 + 2*a_22*t  + 3*a_23*t.^2;
+% 
+%         end
+%    
+%     else
+%      
+%         zd0 =  a_11 + 2*a_12*t + 3*a_13*t.^2;
+%         phid0 =  a_21 + 2*a_22*t  + 3*a_23*t.^2;
+% 
+%     end
+    theta0 = a_10;
+    thetad0 = a_11;
+    phid0 = a_21;
+    %evaluate inpulse ( the integral of the gaussian is 1) 
     Fun = m*l*thetad0;
     Fut = m*l*sin(theta0)*phid0;
       
