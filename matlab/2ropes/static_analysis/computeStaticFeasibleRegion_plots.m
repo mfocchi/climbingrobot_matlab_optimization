e2349e38467462f7d5295e1e8df00fc3d92ad0d1
Michele Focchi <michele.focchi@iit.it>
2024-01-09 15:37:42 +0100

    restored old numerical analysis

--- matlab/2ropes/static_analysis/computeStaticFeasibleRegion_plots.m
+++ matlab/2ropes/static_analysis/computeStaticFeasibleRegion_plots.m
@@ -1,4 +1,4 @@
-
+close all
 
 baseline = 0.4;
 wall_clearance = 0.5;
@@ -9,15 +9,17 @@ max_feasible_pos_baseline=[];
 baseline_vec = [];
 
 % plot according to base_line
-for rope_length=5:1:10
-    for baseline=0.3:0.05:1.2    
+for rope_length=2:0.5:10
+    for baseline=0.3:0.02:1.2    
         pos = getMaxFeasiblePoint(baseline, mu, wall_clearance, rope_length); 
         max_feasible_pos_baseline = [max_feasible_pos_baseline pos];
         baseline_vec = [baseline_vec baseline];
     end
 end
+%old not projected
+%plot_static_region(max_feasible_pos_baseline(1,:), max_feasible_pos_baseline(2,:), max_feasible_pos_baseline(3,:), mu, baseline_vec, 'baseline [m]', [5, 45]);    
+plot_static_region2D(max_feasible_pos_baseline(1,:), max_feasible_pos_baseline(2,:), max_feasible_pos_baseline(3,:), mu, baseline_vec, 'baseline [m]', [5, 45]);
     
-plot_static_region(max_feasible_pos_baseline(1,:), max_feasible_pos_baseline(2,:), max_feasible_pos_baseline(3,:), mu, baseline_vec, 'baseline [m]', [5, 45]);
 %save the plot
 set(gcf, 'Paperunits' , 'centimeters')
 set(gcf, 'PaperSize', [25 20]);
@@ -27,15 +29,17 @@ print(gcf, '-dpdf',strcat('static_analysis_baseline.pdf'),'-painters')
 max_feasible_pos_clearance=[];
 wall_clearance_vec = [];
 % plot according to wall_clearance
-for rope_length=5:1:10
-    for wall_clearance=0.3:0.05:1.2    
+for rope_length=2:0.5:10
+    for wall_clearance=0.3:0.02:1.2    
         pos = getMaxFeasiblePoint(baseline, mu, wall_clearance, rope_length); 
         max_feasible_pos_clearance = [max_feasible_pos_clearance pos];
         wall_clearance_vec = [wall_clearance_vec wall_clearance];
     end
 end    
+%old not projected
+%plot_static_region(max_feasible_pos_clearance(1,:), max_feasible_pos_clearance(2,:), max_feasible_pos_clearance(3,:), mu, wall_clearance_vec, 'wall clearance [m]', [5, 45]);
+plot_static_region2D(max_feasible_pos_clearance(1,:), max_feasible_pos_clearance(2,:), max_feasible_pos_clearance(3,:), mu, wall_clearance_vec, 'clearance [m]', [5, 45]);
 
-plot_static_region(max_feasible_pos_clearance(1,:), max_feasible_pos_clearance(2,:), max_feasible_pos_clearance(3,:), mu, wall_clearance_vec, 'wall clearance [m]', [5, 45]);
 %save the plot
 set(gcf, 'Paperunits' , 'centimeters')
 set(gcf, 'PaperSize', [25 20]);
